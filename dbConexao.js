f5a24a8b7ff22c2aa8802a3c98b995c45ca5ad6d
mateusvg <mateusvitorino@gmail.com>
2022-04-13 16:08:53 -0300

    Update dbConexao.js

--- dbConexao.js
+++ dbConexao.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 //DB CONNECTION
 const conn = mysql.createPool({
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'bb3bd2b935de69',
-    password: 'fb7aa1ef',
-    database: 'heroku_fb1e780812f7b81',
+    host: '',
+    user: '',
+    password: '',
+    database: '',
     multipleStatements: true
 });
 
